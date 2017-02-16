f843df8eccbb5107a73d7fd6be9b5791eace157a
Evan <2686465170@qq.com>
2017-02-16 14:22:54 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const http = require('http')
+
+const server = http.createServer((req,res) => {
+    res.end('hello world')
+});
+
+server.listen(3000,()=>{
+    console.log('Server runing successful')
+})
