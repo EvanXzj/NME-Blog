6a6efa084da4e5537b23b4e77eea92f58f264106
Evan <2686465170@qq.com>
2017-02-16 14:28:57 +0800

    modified app.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 'use strict'
 
-const http = require('http')
+const http = require('http'),
+      port = 3000,
+      hostName = '127.0.0.1'
 
 const server = http.createServer((req,res) => {
-    res.end('hello world')
-});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+})
 
-server.listen(3000,()=>{
-    console.log('Server runing successful')
+server.listen(port,hostName,() => {
+    console.log(`Server runing at http://${hostName}:${port}`)
 })
