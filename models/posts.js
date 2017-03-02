34d9a4017bf62aec5ebce15bf859785b6d52b7c1
Evan <2686465170@qq.com>
2017-03-02 10:45:34 +0800

    文章页完成

---
+++ models/posts.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const marked = require('marked')
+const Post = require('../lib/mongo').Post
+
+// 将 post 的 content 从 markdown 转换成 html
+Post.plugin('contentToHtml',{
+    afterFind:(posts) => {
+        return posts.map(function(post){
+            post.content = marked(post.content)
+            return post
+        })
+    },
+    afterFindOne:(post) => {
+        if(post){
+            post.content = marked(post.content)
+        }
+        return post
+    }
+})
+
+module.exports = {
+    
+    // 创建一篇文章
+    create:(post) => {
+        return Post.create(post).exec()
+    },
+
+    // 通过文章 id 获取一篇文章
+    getPostById:(postId) => {
+        return Post.findOne({_id:postId}).populate({path:'author',model:'User'})
+                   .addCreatedAt().contentToHtml().exec()
+    },
+
+    // 按创建时间降序获取所有用户文章或者某个特定用户的所有文章
+    getPosts:(author) => {
+        var query = {}
+        if(author){
+            query.author = author
+        }
+        return Post.find(query).populate({ path: 'author', model: 'User' }).sort({_id:-1})
+                   .addCreatedAt().contentToHtml().exec()
+    },
+
+    // 通过文章 id 给 pv 加 1
+    incPv:(postId) => {
+        return Post.update({_id:postId},{$inc:{pv:1}}).exec()
+    },
+
+    // 通过文章 id 获取一篇原生文章（编辑文章）
+    getRowPostById:(postId) => {
+        return Post.findOne({_id:postId}).populate({path:'author',model:'User'}).exec()
+    },
+
+    // 通过用户 id 和文章 id 更新一篇文章
+    updatePostById:(postId,author,data) => {
+        return Post.update({author:author,_id:postId},{$set:{content:data}}).exec()
+    },
+
+    // 通过用户 id 和文章 id 删除一篇文章
+    delPostById:(postId,author) => {
+        return Post.remove({author:author,_id:postId}).exec()
+    }
+}
